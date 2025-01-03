58de173db92f413b6117ace5c9fb32b7c5278e02
Pieter Belmans <pieterbelmans@gmail.com>
2025-01-03 16:18:39 +0100

    build: improve setup script

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     entry_points = {
         "console_scripts": ["mr2bib = mr2bib:main"]
     },
-    install_requires = ["pybtex"],
+    install_requires = ["pybtex", "requests", "fake-useragent"],
     license = "BSD",
     classifiers = [
         "Programming Language :: Python :: 2",
